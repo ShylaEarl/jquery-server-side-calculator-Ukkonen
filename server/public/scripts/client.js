d7654da870ecb4ab11d9b39e03ca083591586e54
Shyla Earl <shyla.earl@gmail.com>
2020-10-11 10:26:15 -0500

    css hooked up, starting submitCalculation funtion

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,17 @@ console.log('js');
 
 $(document).ready(onReady);
 
+let num1 = '';
+let num2 ='';
+let operator = '';
+let total = '';
+
 function onReady(){
     console.log('jQuery');
-    
+    $("#submit").on('click', submitCal);
+}
+
+function submitCal(num1, num2){
+    console.log('in submit cal');
+ if(num1)
 }
--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
-const express = require('express'); //require express - gives us a function
-const bodyParser = require('body-parser'); //body parser 'parses' through data from server? client? 
+const express = require('express'); //express gives us a function
+const bodyParser = require('body-parser'); //body parser 'parses' through data and sends it in a little package
 
-//create an instance of express by calling the function
-//returned above - gives us an object
-const app = express();
-const PORT = 5000;
+const app = express(); //create an instance of express by calling the function
+const PORT = 5000; //the port we are operating on/through
 
-//express static file serving - public is the folder name
-app.use(express.static('server/public'));
+app.use(express.static('server/public')); //serves up our static files
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true})); //allows us to find data via urls
 
 
 
-// how to write an annonymous function. Also you need this to 'port' to the DOM
-app.listen(PORT, () => {
-    console.log('Up and running on port:', PORT); //keep this console.log so you know things are working
+
+app.listen(PORT, () => { //tells us the port is open for business
+    console.log('Up and running on port:', PORT); //keep this console.log so you know the port is working
   });
