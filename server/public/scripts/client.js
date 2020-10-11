bd842eb2387af6304518a29e6d0b887772dcc727
Shyla Earl <shyla.earl@gmail.com>
2020-10-11 11:26:50 -0500

    woring on POST request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,17 +2,37 @@ console.log('js');
 
 $(document).ready(onReady);
 
-let num1 = '';
-let num2 ='';
 let operator = '';
 let total = '';
 
 function onReady(){
     console.log('jQuery');
-    $("#submit").on('click', submitCal);
+    $(".operator").on("click", opFun);
+    $("#submitButton").on('click', submitCal); //click event to calculate calculation
+    
 }
 
-function submitCal(num1, num2){
-    console.log('in submit cal');
- if(num1)
+function opFun(){
+    operator = $(this).data('value');
+    console.log('in operator function', operator);
+}
+
+function postNumbers(){
+    let num1 = $('#num1').val();
+    let num2 = $('#num2').val(); //inside ajax post in client inside of data object to send to the server
+
+    $.ajax({
+        method: 'POST',
+        url: '/submitNumbers',
+        data: {
+            num1: num1,
+            num2: num2,
+            operator: operator
+        }
+    }).then(function(response){
+        console.log('response', response)
+        // add getfunction here ();
+    }).catch(function(error){
+        alert(error);
+    });
 }
--- server/server.js
+++ server/server.js
@@ -8,7 +8,42 @@ app.use(express.static('server/public')); //serves up our static files
 
 app.use(bodyParser.urlencoded({extended: true})); //allows us to find data via urls
 
+app.post('/submitNumbers', (req, res) => {
+  console.log('hello from post', req.body); // req.body is related to body parser. important to add console.log with descriptive strings in each app to debug/know where things are/coming from
+  res.sendStatus(200);
+});
 
+//write post request call submitCal from inside POST request 
+//(POST to server or GET things from server)
+//this is how you get values and operators from client to server
+// when DOM '=' is clicked, the calculation is run
+function submitCal(num1, num2){
+    console.log('in submit cal', submitCal);
+ switch (operator){
+     case '+':
+         total = +num1 + +num2;
+         submitButton(total);
+         break;
+    }
+ switch (operator){
+     case '-':
+         total = +num1 - +num2;
+         submitButton(total);
+         break;
+    }
+ switch (operator){
+     case '*':
+         total = +num1 * +num2;
+         submitButton(total);
+         break;
+    }
+ switch (operator){
+     case '/':
+         total = +num1 / +num2;
+         submitButton(total);
+         break;
+    }
+}
 
 
 app.listen(PORT, () => { //tells us the port is open for business
